6f856a31674ea9df72754183b80b5ff6d46a0f5c
abdallah <abdallhamohamed116@gmail.com>
2023-04-25 17:55:25 +0200

    adding animation to text

--- lib/features/splash/presentation/views/widgets/splash_screen_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_screen_body.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/assets.dart';
 
-class SplashScreenBody extends StatelessWidget {
+class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({super.key});
+
+  @override
+  State<SplashScreenBody> createState() => _SplashScreenBodyState();
+}
+
+class _SplashScreenBodyState extends State<SplashScreenBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+ 
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +31,24 @@ class SplashScreenBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo,),
-        const SizedBox(height: 6,),
-       const  Text('Read Free Books ',textAlign: TextAlign.center,)
+        Image.asset(
+          AssetsData.logo,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context,_) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                'Read Free Books ',
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+        )
       ],
     );
   }
